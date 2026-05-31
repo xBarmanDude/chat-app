0f6a5bc37ba27d710c0038f9d090eb0ea6188a61
xBarmanDude <carryislive3311@gmail.com>
2026-05-31 23:22:42 +0530

    fix socketio production render

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
+import eventlet
+
+eventlet.monkey_patch()
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
 @app.route('/')
 def index():
@@ -13,4 +16,4 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=10000)
