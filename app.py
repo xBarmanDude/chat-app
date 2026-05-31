9d6f868767c0507f00a88ada64679d93cf1f3e60
xBarmanDude <carryislive3311@gmail.com>
2026-05-31 16:41:57 +0530

    chat app ready

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+messages = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send', methods=['POST'])
+def send():
+    msg = request.json['msg']
+    messages.append(msg)
+    return "OK"
+
+@app.route('/get')
+def get():
+    return jsonify(messages)
+
+if __name__ == "__main__":
+    app.run(debug=True)
