e9d400e0c879cf08e4d5193042209f1c2157e087
xBarmanDude <carryislive3311@gmail.com>
2026-05-31 23:39:29 +0530

    fix socket without eventlet

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
-import eventlet
-
-eventlet.monkey_patch()
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+
+# IMPORTANT: no eventlet
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
 @app.route('/')
 def index():
