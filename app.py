d85de2059e000f40706089f7ee48ec454fb717c4
xBarmanDude <carryislive3311@gmail.com>
2026-05-31 23:10:51 +0530

    upgrade real time chat

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-
-messages = []
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/send', methods=['POST'])
-def send():
-    msg = request.json['msg']
-    messages.append(msg)
-    return "OK"
-
-@app.route('/get')
-def get():
-    return jsonify(messages)
+@socketio.on('message')
+def handle_message(msg):
+    send(msg, broadcast=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    socketio.run(app, host="0.0.0.0", port=10000)
